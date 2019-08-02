7322adcc4914d3832d3aa656e6c112fd0e17fc0c
Damian Lewandowski <damian.lewandowski1@outlook.com>
2019-08-02 09:34:31 +0200

    Admin Pages CZESC.1

---
+++ Areas/Admin/Controllers/PagesControllerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShop.Areas.Admin.Controllers
+{
+    public class PagesControllerController : Controller
+    {
+        // GET: Admin/PagesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/Pages/PageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CmsShop.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CmsShop.Models.ViewModels.Pages
+{
+    public class PageViewModel
+    {
+
+        public PageViewModel()
+        {
+        }
+        public PageViewModel(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSidebar = row.HasSidebar;
+
+        }
+        public int Id { get; set; }
+        [Required]                                          // To oznacza ze pole jest wymagane
+        [StringLength(50,MinimumLength =3)]                 // Maksymalna wartość tego pola to 50, a minimalna to 3
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]     // Maksymalna wartość tego pola to int.MaxValue(Ponieważ w DB ustawiłem MAX),
+                                                            // a minimalna to 3
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
