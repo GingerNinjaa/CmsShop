4799a6082ae6e39f52b2d5190ace2a09b5af11f5
Damian Lewandowski <damian.lewandowski1@outlook.com>
2019-08-02 10:23:04 +0200

    Admin Pages Czesc 1

---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,30 @@
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShop.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/PagesController
+        public ActionResult Index()
+        {
+
+            // Deklaracja listy PageViewModel
+            List<PageViewModel> pagesList;
+
+            //Inicjalizacja listy
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageViewModel(x)).ToList();
+            }
+          
+            // Zwracamy strony do widoku 
+            return View(pagesList);
+        }
+    }
+}
