ddd9cdce2ab91b1c26c620a72434bd838f8fcf61
Damian Lewandowski <damian.lewandowski1@outlook.com>
2019-08-26 09:38:45 +0200

    Front Pages czesc 1 dodanie kontrolera i widoku i tras

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,53 @@
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShop.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{pages}
+        public ActionResult Index(string page = "")
+        {
+            // ustawiamy adres naszej strony 
+            if (page == "")
+                page = "home";
+
+            // delkarujemy pagevm i pageDTO
+            PageViewModel model;
+            PageDTO dto;
+            // Sprawdzamy czy strona istnieje 
+            using (Db db = new Db())
+            {
+                //Jeśli strona nie istnieje to przekierowujemy ją na strone główną 
+
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))                
+                    return RedirectToAction("Index", new { page = "" });                
+            }
+
+            // Pobieramy pageDTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // ustawiamy tytuł naszej strony 
+            ViewBag.PageTitle = dto.Title;
+
+            // sprawdzamy czy strona ma sidebar 
+            if (dto.HasSidebar == true)
+                ViewBag.Sidebar = "Tak";
+            else
+                ViewBag.Sidebar = "Tak";
+            // inicjalizujemy pageVM
+            model = new PageViewModel(dto);
+
+            // zwracamy widok z pageVM
+            return View(model);
+        }
+    }
+}
