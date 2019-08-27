49f61b992ceb7d48cbb8801d8341944be1ed45a0
Damian Lewandowski <damian.lewandowski1@outlook.com>
2019-08-27 12:51:33 +0200

    Koszyk czesc 3 dodawanie koszyka

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using CmsShop.Models.ViewModels.Cart;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Cart;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace CmsShop.Controllers
             var cart = Session["cart"] as List<CartViewModel> ?? new List<CartViewModel>();
 
             //Sprawdzamy czy nasz koszyk jest pusty
-            if (cart.Count ==0 || Session["cart"] == null)
+            if (cart.Count == 0 || Session["cart"] == null)
             {
                 ViewBag.Message = "Twój koszyk jest pusty";
                 return View();
@@ -58,9 +59,60 @@ namespace CmsShop.Controllers
             else
             {
                 // ustawiamy ilośc i cena na 0
-                qty =  0;
+                qty = 0;
                 price = 0m;
             }
+
+            return PartialView(model);
+        }
+
+        public ActionResult AddToCartPartial(int id)
+        {
+            //Inicjalizacja CartVMlist
+            List<CartViewModel> cart = Session["cart"] as List<CartViewModel> ?? new List<CartViewModel>();
+
+            // inicjalizacja CartViewModel
+            CartViewModel model = new CartViewModel();
+
+            using (Db db = new Db())
+            {
+                // pobieramy produkt 
+                ProductDTO product = db.Products.Find(id);
+
+                //sprawdzamy czy ten produkt jest w już w koszyku
+                var productInCart = cart.FirstOrDefault(x => x.ProductId == id);
+
+                // w zalezności od tego czy produkt jest w koszyku go dodajemy lub zwiekszamy ilośc 
+                if (productInCart == null)
+                {
+                    cart.Add(new CartViewModel()
+                    {
+                        ProductId = product.Id,
+                        ProductName = product.Name,
+                        Quantity = 1,
+                        Price = product.Price,
+                        Image = product.ImageName
+                    });
+                }
+                else
+                {
+                    productInCart.Quantity++;
+                }
+            }
+            // pobieramy  całkowite wartość ilości i ceny i dodajemy do modelu 
+            int qty = 0;
+            decimal price = 0m;
+            foreach (var item in cart)
+            {
+                qty += item.Quantity;
+                price += item.Quantity * item.Price;
+            }
+
+            model.Quantity = qty;
+            model.Price = price;
+
+            //Zapis w sesji
+            Session["cart"] = cart;
 
             return PartialView(model);
         }
