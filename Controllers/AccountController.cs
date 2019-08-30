25d405d9495b8cfc50a34e6f9d6aaad8a51c8a99
Damian Lewandowski <damian.lewandowski1@outlook.com>
2019-08-30 10:27:46 +0200

    Rejestracja i logowanie czesc 4 tworzenie użytkownika

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,12 +15,15 @@ namespace CmsShop.Controllers
         {
             return Redirect("~/account/login");
         }
+
         // GET: Account/create-account
+        [HttpGet]
         [ActionName("create-account")]
         public ActionResult CreateAccount()
         {
             return View("CreateAccount");
         }
+
         // GET: Account/login
         public ActionResult Login()
         {
@@ -31,6 +36,69 @@ namespace CmsShop.Controllers
 
             // zwracamy widok 
             return View();
+        }
+
+        // Post: Account/create-account
+        [ActionName("create-account")]
+        [HttpPost]
+        public ActionResult CreateAccount(UserViewModel model)          //Rejestracja 
+        {
+            // sprawdzenie model state
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAccount", model);
+            }
+
+            //Sprawdzenie hasła
+            if (!model.Password.Equals(model.ConfirmPassword))
+            {
+                ModelState.AddModelError("", "Hasła do siebie nie pasują");
+                return View("CreateAccount", model);
+            }
+
+            //Sprawdzenie czy nazwa użytkownika jest unikalna 
+            using (Db db = new Db())
+            {
+                if (db.Users.Any(x => x.UserName.Equals(model.UserName)))
+                {
+                    ModelState.AddModelError("", "Nazwa użytkownika "+ model.UserName + " Jest już zajęta");
+                    model.UserName = "";
+                    return View("CreateAccount", model);
+                }
+                //utworzenie użytkownika 
+                UserDTO usserDTO = new UserDTO()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    EmailAddress = model.EmailAddress,
+                    UserName = model.UserName,
+                    Password = model.Password,
+                };
+
+                //dodawanie użytkownika 
+                db.Users.Add(usserDTO);
+
+                //Zapis na bazie 
+                db.SaveChanges();
+
+                // dodanie roli dla użytkownika 
+                UserRoleDTO userRoleDTO = new UserRoleDTO()
+                {
+                    UserId = usserDTO.Id,
+                    RoleId = 2,
+                };
+
+                //Dodanie roli 
+                db.UserRoles.Add(userRoleDTO);
+
+                //zapis na bazie 
+                db.SaveChanges();
+
+                //TempData komunikat
+                TempData["SM"] = "Jestes tereaz zarejestrowany i możesz się zalogować.";
+            }
+
+            return Redirect("~/account/login");
         }
     }
 }
