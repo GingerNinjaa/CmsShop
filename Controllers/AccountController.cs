28b3f7986d02c21087d2e15ca75776e2d3f21f39
Damian Lewandowski <damian.lewandowski1@outlook.com>
2019-08-30 09:34:43 +0200

    Rejestracja i logowanie użytkowników czesc 3 dodanie widoku logowania

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -11,15 +11,26 @@ namespace CmsShop.Controllers
         // GET: Account
         public ActionResult Index()
         {
-            return View();
+            return Redirect("~/account/login");
         }
         // GET: Account/create-account
         [ActionName("create-account")]
         public ActionResult CreateAccount()
         {
-
             return View("CreateAccount");
+        }
+        // GET: Account/login
+        public ActionResult Login()
+        {
+            // sprawdzanie czy użytkownik nie jest już zalogowany
+            string userName = User.Identity.Name;
 
+            // Jeśli jest zalogowany przekieruje go na user-profile
+            if (!string.IsNullOrEmpty(userName))
+                return RedirectToAction("user-profil");
+
+            // zwracamy widok 
+            return View();
         }
     }
 }
